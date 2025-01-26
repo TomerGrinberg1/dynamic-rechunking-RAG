576021d9ad3ad37d839a2ffabfa50152c3aacce7
Hadar-Sug <81767379+Hadar-Sug@users.noreply.github.com>
2025-01-26 19:42:30 +0200

    Update DeepSeek_interface.py

--- scripts/DeepSeek_interface.py
+++ scripts/DeepSeek_interface.py
@@ -1,7 +1,7 @@
 
 from openai import OpenAI
 
-client = OpenAI(api_key="sk-6aa4c88ba3134cf9827695e32e2fd7c9", base_url="https://api.deepseek.com")
+client = OpenAI(api_key="", base_url="https://api.deepseek.com")
 
 response = client.chat.completions.create(
     model="deepseek-chat",
