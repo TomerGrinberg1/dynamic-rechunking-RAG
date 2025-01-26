672346b14723503b74e6b9873146acf2b3bfb683
Tomer Grinberg <tomerg@copyleaks.com>
2025-01-26 20:37:16 +0200

    update deepseek instead of llama

--- scripts/RAG_orchestrator.py
+++ scripts/RAG_orchestrator.py
@@ -4,10 +4,11 @@ from tqdm import tqdm
 import json
 from  Retrieval import    RetrievalService
 from  expansion_service import ExpansionService
-from ollama_interface import OllamaInterface
+from LLM_interface import DeepSeekInterface
 import pandas as pd
 import os
 import nltk
+import yaml
 
 def load_data_from_json(file_path):
     """
@@ -32,15 +33,12 @@ def create_rag_prompt(question, retrieved_context, k=3):
     passages = "\n".join([f"passage {i + 1}: {retrieved_context[i][0]}" for i in range(min(k, len(retrieved_context)))])
     
     return f"""
-            <|begin_of_text|><|start_header_id|>system<|end_header_id|>
-
-            Answer the question based on the given passages below. Keep the answer concise.
-            <|eot_id|><|start_header_id|>user<|end_header_id|>
-
+            Answer the following question based on the given passages below: Keep the answer concise,  use plain text only.
+           
             ## Passages:
             {passages}
 
-            ## Question: {question} <|eot_id|><|start_header_id|>assistant<|end_header_id|>
+            ## Question: {question} 
 
             ##Answer:
             """
@@ -100,15 +98,24 @@ model = SentenceTransformer('intfloat/e5-base-v2')
 # Initialize Pinecone
 pinecone_index_name="dynamic"
 pinecone_api_key="pcsk_5SLo1E_SPpKBieD8wQRuRf9G7xfYrVWDLWUAkfRC3X5xjMpDhy7j8CH3SEen8kJiNbMjav"
+with open("keys.yaml", 'r') as stream:
+    try:
+        config = yaml.safe_load(stream)
+        key=config["api_key"]
+
+    except yaml.YAMLError as exc:
+        print(exc)
+        exit()
+
 data_path="/home/tomer/learning/dynamic-rechunking-RAG/data/5000_passage_to_location.json"
 retrieval_service = RetrievalService(pinecone_index_name=pinecone_index_name, pinecone_api_key=pinecone_api_key)
 expansion_service = ExpansionService()
-ollama_service=OllamaInterface()
+llm_service=DeepSeekInterface(api_key=key)
 data=retrieval_service.load_data(data_path)
 # retrieval_service.index_and_upsert_documents(data, to_index_flag=True, to_upsert_flag=True, batch_size=64)
 queries=get_queries("/home/tomer/learning/dynamic-rechunking-RAG/data/short_answer_5000_rows.parquet")
 real_answers = get_real_answer_list('/home/tomer/learning/dynamic-rechunking-RAG/data/short_answer_5000_rows.parquet') 
-n_neighbors_list = [1,2]
+n_neighbors_list = [0,1,2,5,10]
 
 hyperparams_status_file = "/home/tomer/learning/dynamic-rechunking-RAG/data/hyperparams_status.json"
 accuracy_file = "/home/tomer/learning/dynamic-rechunking-RAG/data/hyperparams_accuracy.json"
@@ -147,7 +154,7 @@ for n_neighbors in tqdm(remaining_hyperparams["n_neighbors"], desc="n_neighbors 
 
         rerenaked_passages = expansion_service.rerank_chunks(query, distilled_passages)
         rag_prompt = create_rag_prompt(query, rerenaked_passages)
-        response = ollama_service.send_message(rag_prompt)
+        response = llm_service.send_message(rag_prompt)
         
         # Extract real answer and check correctness using the provided method
         real_answer_list = real_answers[i]
@@ -156,21 +163,26 @@ for n_neighbors in tqdm(remaining_hyperparams["n_neighbors"], desc="n_neighbors 
             correct_answers += 1
             
                 # Save query-specific data
-        results.append({
+        result_data = {
             "query": query,
             "llm_response": response,
             "real_answers": real_answer_list,
             "is_correct": is_correct
-        })
+        }
+        results.append(result_data)
+        
+        # Save intermediate results for every query
+        result_file = f"/home/tomer/learning/dynamic-rechunking-RAG/data/results_n_neighbors_{n_neighbors}.json"
+        with open(result_file, "w") as json_file:
+            json.dump(results, json_file, indent=4)
+
+   
+
+ 
 
     # Compute accuracy for current n_neighbors
     accuracy = correct_answers / total_queries if total_queries > 0 else 0.0
     accuracy_data[n_neighbors] = accuracy
-
-        # Save results for current n_neighbors
-    result_file = f"/home/student/dynamic-rechunking-RAG/data/results_n_neighbors_{n_neighbors}.json"
-    with open(result_file, "w") as json_file:
-        json.dump(results, json_file, indent=4)
     
     # Save progress
     with open(accuracy_file, "w") as json_file:
